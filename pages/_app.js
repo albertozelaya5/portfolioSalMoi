ddc6c9c1296139ff9a47b25a97227f865f8caa3d
ajzmbhc <ajzelaya@banhcafe.hn>
2025-08-14 14:11:43 -0600

    fix: dynamic imports in same pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import Preloader from "@/src/components/Preloader";
 import { Fragment } from "react";
-import StaticHead from "./StaticHead";
+import Head from "next/head";
+import Preloader from "@/src/components/Preloader";
 
 const App = function ({ Component, pageProps }) {
   return (
@@ -9,6 +9,37 @@ const App = function ({ Component, pageProps }) {
       <Preloader />
       <Component {...pageProps} />
     </Fragment>
+  );
+};
+
+const StaticHead = () => {
+  return (
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="author" content="Moisés Aguilar" />
+      <meta name="robots" content="index, follow" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="https://moisesaguilar.dev/images/profile.webp" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content="https://moisesaguilar.dev/images/profile.webp" />
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      {/* Template Google Fonts */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Livvic:wght@100;200;300;400;500;600;700&family=Oswald:wght@400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
+      <link rel="stylesheet" href="css/devicon.min.css" />
+      <link rel="stylesheet" href="css/all.min.css" />
+      <link rel="stylesheet" href="css/bootstrap.min.css" />
+      <link rel="stylesheet" href="css/swiper-bundle.min.css" />
+      <link rel="stylesheet" href="css/animate.min.css" />
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css" />
+      <link rel="stylesheet" href="css/style.css" />
+      <link rel="stylesheet" type="text/css" href="css/styleswitcher.css" />
+    </Head>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Fragment, useEffect, useState } from "react";
+import Head from "next/head";
 
 import Header from "@/src/components/Header";
 import ScrollBar from "@/src/components/ScrollBar";
@@ -10,7 +11,6 @@ import Facts from "@/src/components/sections/Facts";
 import Home from "@/src/components/sections/Home";
 import Portfolio from "@/src/components/sections/Portfolio";
 import Separator from "@/src/components/Separator";
-import DynamicMetaHead from "./DynamicMetaHead";
 import { jqueryFunction } from "@/src/utilits";
 
 const Index = function ({ initialLanguage = "es" }) {
@@ -20,9 +20,54 @@ const Index = function ({ initialLanguage = "es" }) {
     jqueryFunction();
   }, []);
 
+  const metaData = {
+    es: {
+      title: "Moisés Aguilar | Arquitecto de Software",
+      description:
+        "Portafolio de Moisés Aguilar, Arquitecto de Software especializado en el diseño y desarrollo de aplicaciones web robustas, escalables y de alto rendimiento.",
+      keywords:
+        "arquitecto de software, desarrollador senior, react, javascript, node.js, arquitectura de software, portfolio, moises aguilar, web developer",
+      ogTitle: "Moisés Aguilar | Arquitecto de Software",
+      ogDescription:
+        "Portafolio de Moisés Aguilar, Arquitecto de Software especializado en el diseño y desarrollo de aplicaciones web robustas y escalables.",
+      ogUrl: "https://moisesaguilar.dev/",
+      ogLocale: "es_ES",
+      twitterTitle: "Moisés Aguilar | Arquitecto de Software",
+      twitterDescription:
+        "Portafolio de Moisés Aguilar, Arquitecto de Software especializado en el diseño y desarrollo de aplicaciones web.",
+    },
+    en: {
+      title: "Moisés Aguilar | Software Architect",
+      description:
+        "Portfolio of Moisés Aguilar, a Software Architect specializing in the design and development of robust, scalable, and high-performance web applications.",
+      keywords:
+        "software architect, senior developer, react, javascript, node.js, software architecture, portfolio, moises aguilar, web developer",
+      ogTitle: "Moisés Aguilar | Software Architect",
+      ogDescription:
+        "Portfolio of Moisés Aguilar, a Software Architect specializing in designing and developing robust and scalable web applications.",
+      ogUrl: "https://moisesaguilar.dev/en",
+      ogLocale: "en_US",
+      twitterTitle: "Moisés Aguilar | Software Architect",
+      twitterDescription:
+        "Portfolio of Moisés Aguilar, a Software Architect specializing in designing and developing web applications.",
+    },
+  };
+
+  const currentMeta = metaData[language];
+
   return (
     <Fragment>
-      <DynamicMetaHead language={language} />
+      <Head>
+        <title>{currentMeta.title}</title>
+        <meta name="description" content={currentMeta.description} />
+        <meta name="keywords" content={currentMeta.keywords} />
+        <meta property="og:title" content={currentMeta.ogTitle} />
+        <meta property="og:description" content={currentMeta.ogDescription} />
+        <meta property="og:url" content={currentMeta.ogUrl} />
+        <meta property="og:locale" content={currentMeta.ogLocale} />
+        <meta name="twitter:title" content={currentMeta.twitterTitle} />
+        <meta name="twitter:description" content={currentMeta.twitterDescription} />
+      </Head>
       <div className="page-content">
         <Header language={language} setLanguage={setLanguage} />
         <div id="wrapper">
